710dd1e22d34b36c5d9644a29e011c657b22d3f9
gom1n <alice43677@naver.com>
2023-08-07 12:32:46 +0900

    init commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var mySwiper = new Swiper('.swiper-container', {
+        loop: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+});
