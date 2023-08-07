68c83ea1de372eca60f86e3fa2c8b705627041ec
gom1n <alice43677@naver.com>
2023-08-07 13:57:15 +0900

    Feat: 상단, 하단바 커스텀

--- script.js
+++ script.js
@@ -5,5 +5,9 @@ document.addEventListener("DOMContentLoaded", function() {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
     });
 });
